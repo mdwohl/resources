c40e7dfde5ef1bd7cded1213957db85412e22721
mdwohl <mdwohl@gmail.com>
2020-06-24 15:34:47 -0700

    more script

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var location = prompt('Are you located in the Puget Sound? Please answer YES or NO.')
+var welcome
+var dogs
+
+if location = 'YES' {
+    welcome = 'See links below for adoption resources.'
+} else {
+    Response.sendRedirect("https://www.google.com")
+
+    if (dogs > 4) {
+        welcome = 'That is too many!';
+    } else if (dogs < 4) {
+        welcome = 'That is a good number of dogs.';
+    }
