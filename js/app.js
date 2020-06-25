6f07744103aadb4740ca3a3c763724c61e186b1f
mdwohl <mdwohl@gmail.com>
2020-06-25 12:27:34 -0700

    functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
+'use strict'
+
+function numberDogs(){
 var dogs = prompt('How many dogs do you want?');
 var welcome;
 
-    if (dogs > 4) {
-        welcome = 'Your number is too many! You may want to reconsider.';
+    if (dogs >= 4) {
+        welcome = ' dogs is too many! You may want to reconsider.';
+    } else if (dogs < 4) {
+        welcome = ' is a good number of dogs.';
     } else {
-        welcome = 'Your number is a good number of dogs.';
+        alert('Please enter a valid number of dogs!');
     }
-    document.write(welcome);
+    document.write(dogs + welcome);
+}
 
-    
+numberDogs()
+dogTime()
+
+function dogTime(){
+var today = new Date();
+var hourNow = today.getHours();
+var dogTime
+
+    if (hourNow < 18) {
+        dogTime = " between 10AM and 6PM. Shelters are open and it's a good time to get a dog!";
+    }   else if (hourNow < 10) {
+        dogTime = " before 10AM or after 6PM -- wait until the shelters are open to get a dog!";
+    }
+    document.write(' Ready to get a dog? It is now' + dogTime)
+}
+
+confirm('A dog is a lot of work! Press OK to confirm your commitment to your pup.');
